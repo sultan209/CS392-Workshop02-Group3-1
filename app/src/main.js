0eb25f91aa10812e98868dd54168e63f9ace4772
Ahmed AlSardi <A7Dpy@outlook.com>
2021-12-10 23:53:41 +0300

    Create a Dockerfile image && adding dotenv

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,10 @@
 const express = require("express")
+require("dotenv").config()
 const foodPrediction = require("./food-prediction")
 
+
 const app = express()
+const port = process.env.PORT || 8000
 
 app.use(express.json())
 
@@ -20,6 +23,6 @@ app.get("/predict", async (req, res) => {
 })
 
 
-app.listen(8000, () => {
-    console.log("App listen at port 8000")
+app.listen(port, () => {
+    console.log(`App listen at port: ${port}`)
 })
